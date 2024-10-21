dfdc78c0d6beb93102ea6394c5e168621583b99f
andriandrian <naird.id@gmail.com>
2024-10-21 09:17:18 +0700

    update: fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./ui/navbar";
+// import Navbar from "./ui/navbar";
 import { inter } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
